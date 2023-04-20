7f1c90189473f8fd3ec50ec57ce35d74b5a0c54d
ihmdevcont <121264523+ihmdevcont@users.noreply.github.com>
2023-04-19 17:55:23 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,44 @@
+"use client";
+
+import { useAuth } from "@pangeacyber/react-auth";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const {
+    authenticated,
+    loading,
+    error,
+    user,
+    client,
+    login,
+    logout,
+    getToken,
+  } = useAuth();
+
   return (
     <main className={styles.main}>
       <div>
+        {!authenticated && (
+          <div className="card">
+            <h2>
+              Welcome to your brand new NextJS app integrated with{" "}
+              <a href="https://pangea.cloud/">Pangea</a>
+            </h2>
+          </div>
+        )}
+        {authenticated && (
+          <div className="card">
+            <h2>
+              You have been successfully authenticated by{" "}
+              <a href="https://pangea.cloud/">Pangea AuthN Service</a>
+            </h2>
+          </div>
+        )}
+
         <div className="card">
-          Welcome to your brand new NextJS app integrated with Pangea AuthN
-          service for authentication.
-        </div>
-        <div className="card">
-          You can read more about Pangea AuthN service here
-          https://pangea.cloud/docs/
+          You can read more about <a href="https://pangea.cloud/">Pangea</a>{" "}
+          AuthN service
+          <a href="https://pangea.cloud/docs/authn"> here</a>
         </div>
       </div>
     </main>
